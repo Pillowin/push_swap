3d5133f5ce985c98cc9cebc51b34f0f267f88423
Antoine GAUTIER <gautier.antoine@gmx.com>
2021-06-02 15:03:13 +0200

    fix(test): test.sh is now working on macos

--- test/test.sh
+++ test/test.sh
@@ -1,29 +1,44 @@
 #! /bin/bash
 
-for i in `seq 1 5000`
+read -p "Please entre the number of lists: " nb_lists
+if ! [[ "$nb_lists" =~ ^[0-9]+$ ]]
+then
+	echo "Sorry positive integers only"
+	exit 1
+fi
+
+read -p "Please entre the minimal number: " min
+if ! [[ "$nb_lists" =~ ^[0-9]+$ ]]
+then
+	echo "Sorry positive integers only"
+	exit 1
+fi
+
+read -p "Please entre the maximal number: " max
+if ! [[ "$nb_lists" =~ ^[0-9]+$ ]]
+then
+	echo "Sorry positive integers only"
+	exit 1
+fi
+
+if [ $min -ge $max ]
+then
+	echo "Max needs to be greater than min"
+	exit 1
+fi
+
+for i in `seq 1 $nb_lists`
 do
-    arg=`shuf -i 1-5000 -n 2000 `
-    arg=`sed -z 's/\n/ /g' <<<"$arg" `
-    ret=`./push_swap $arg`
-    if [ "$ret" ]
-    then
-        error=`echo "$ret" | ./checker $arg 2>&1`
-    else
-        error=`echo -n "$ret" | ./checker $arg 2>&1`
-    fi
-    echo $error >>error
-    if [[ $error != "OK" ]]
-    then 
-         echo $arg >>error
-    fi
-    ret=`echo "$ret" | wc -l`
-    #ret2=`./push_swap2 $arg | wc -l`
-    if [[ $ret -ge 0 ]]
-    then
-        echo " solved in $ret tries"
-    fi
-    #if [[ $ret2 -ge 0 ]]
-    #then
-    #    echo " solved in $ret2 tries" >> log2
-    #fi
+	arg=`shuf -i $min-$max`
+	arg=`echo $arg | tr '\n' ' '`
+	echo -n $arg
+	ret=`./push_swap $arg`
+	if [ "$ret" ]
+	then
+		error=`echo "$ret" | ./checker $arg 2>&1`
+	else
+		error=`echo -n "$ret" | ./checker $arg 2>&1`
+	fi
+	ret=`echo "$ret" | wc -l | tr -d '[:space:]'`
+	echo " solved in $ret tries"
 done
